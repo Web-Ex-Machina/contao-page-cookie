7fbe0b58485febce0aba88891edc831e68e1e586
LupusVII <jln.thirion@gmail.com>
2024-02-19 14:19:36 +0100

    Add bundle file

---
+++ src/ContaoPageCookieBundle.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace ContaoPageCookieBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Configures the bundle.
+ *
+ * @author Web ex Machina <https://www.webexmachina.fr>
+ */
+class ContaoPageCookieBundle extends Bundle
+{
+}
